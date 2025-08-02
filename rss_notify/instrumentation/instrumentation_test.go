3bd765981a0a2da2479c872c727ba800c8dc4169
FKouhai <frandres00@gmail.com>
2025-08-02 22:01:37 +0200

    feat: add instrumentation for rss_notify

---
+++ rss_notify/instrumentation/instrumentation_test.go
@@ -0,0 +1,22 @@
+package instrumentation
+
+import (
+	"os"
+	"testing"
+)
+
+func TestGetTracer(t *testing.T) {
+	tracer := GetTracer("test-tracer")
+	if tracer == nil {
+		t.Error("Expected a valid Tracer object, got nil.")
+	}
+}
+
+func TestInitNoError(t *testing.T) {
+	// nolint
+	os.Setenv("OTEL_EP", "testingotel12:443")
+	_, err := InitTracer()
+	if err != nil {
+		t.Errorf("Expected no error but got %v", err)
+	}
+}
