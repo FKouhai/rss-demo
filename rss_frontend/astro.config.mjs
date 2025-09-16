c95cf80a4641a97561db76ce489c0331258ea3b9
ab2026503 <adrian.barrio.andres@oracle.com>
2025-09-16 10:23:53 +0200

    fix: build and stuff

--- rss_frontend/astro.config.mjs
+++ rss_frontend/astro.config.mjs
@@ -1,9 +1,13 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
+import { loadEnv } from "vite";
 import preact from '@astrojs/preact';
 import node from '@astrojs/node';
 
 import tailwindcss from '@tailwindcss/vite';
+
+const { NODE_TLS_REJECT_UNAUTHORIZED } = loadEnv(process.env.NODE_ENV || 'development', process.cwd(), "");
+process.env.NODE_TLS_REJECT_UNAUTHORIZED = NODE_TLS_REJECT_UNAUTHORIZED;
 
 // https://astro.build/config
 export default defineConfig({
