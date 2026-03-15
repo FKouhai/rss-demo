12bff7a2779e2b6933185cd44d44655e74c1d332
FKouhai <frandres00@gmail.com>
2026-03-15 14:46:03 +0100

    fix(arion-pkgs): after refactoring the flake

--- arion-pkgs.nix
+++ arion-pkgs.nix
@@ -1,1 +1,9 @@
-let flake = (builtins.getFlake (toString ./.)).pkgs; in flake.pkgs
+let
+  lock = builtins.fromJSON (builtins.readFile ./flake.lock);
+  nixpkgs = lock.nodes.nixpkgs.locked;
+  pkgs = import (builtins.fetchTarball {
+    url = "https://github.com/NixOS/nixpkgs/archive/${nixpkgs.rev}.tar.gz";
+    sha256 = nixpkgs.narHash;
+  }) { };
+in
+pkgs
